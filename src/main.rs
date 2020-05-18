c18d72687e585c5d0bb24743d1cb55d56f4e8b2e
Radek Krahl <radek@krahl.pl>
2020-05-18 07:01:52 +0000

    Check local links and [http,https] urls

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use glob::glob;
 use pulldown_cmark::{Event, Parser, Tag};
 use std::fs;
+use std::path::{Path, PathBuf};
+
+const URL_SCHEMAS: &[&str] = &["https://", "http://"];
 
 fn extract_links(md: &str) -> Vec<String> {
     let mut links: Vec<String> = Vec::new();
@@ -14,16 +17,44 @@ fn extract_links(md: &str) -> Vec<String> {
 }
 
 fn main() {
-    for entry in glob("/home/radekk/kide.doc/src//**/*.md").expect("Failed to read glob pattern") {
+    let client = reqwest::blocking::Client::new();
+    for entry in glob("../../kide.doc/src//**/*.md").expect("Failed to read glob pattern") {
         match entry {
             Ok(path) => {
-                println!("{:?}", path.display());
-                let md = fs::read_to_string(path).expect("Cannot read file");
+                let md = fs::read_to_string(&path).expect("Cannot read file");
+
+                println!("FILE: {}", path.display());
+                println!();
+
                 let links = extract_links(&md);
-                println!("{:?}", links);
-                /* FIXME: check links, print output */
+                let parent = path.parent();
+                links.iter().for_each(|l| {
+                    let is_url = URL_SCHEMAS.into_iter().any(|schema| l.starts_with(schema));
+
+                    let result = if !is_url {
+                        let to_check = match parent {
+                            Some(p) => p.join(Path::new(l)),
+                            None => PathBuf::from(l),
+                        };
+                        to_check.exists()
+                    } else {
+                        let res = client.head(l).send();
+                        match res {
+                            Err(_) => false,
+                            _ => true,
+                        }
+                    };
+
+                    if !result {
+                        println!("{}: {}", l, "ERROR");
+                    }
+                });
+
+                println!();
+                println!("{} links checked.", links.len());
             }
             Err(e) => println!("{:?}", e),
         }
+        println!("\n\n");
     }
 }
